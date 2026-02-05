21ff45da1e77f0235dfdcfa069f928b952215e72
william-lett <wlett91@gmail.com>
2026-02-04 22:58:28 -0500

    3-3, updated main.py, added rotation

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
